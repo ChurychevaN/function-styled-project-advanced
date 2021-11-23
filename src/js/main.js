7a7e780eb1df36b4ba88f4b9b0edd87eafb4651d
Your Name <you@example.com>
2021-11-23 13:36:08 +0100

    lesson-16

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,9 @@ import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import checkTextInputs from './modules/checkTextInputs';
-
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded',() => {
-	'use strict';
-
 	modals();
 	sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 	sliders('.main-slider-item', 'vertical');
@@ -15,4 +13,5 @@ window.addEventListener('DOMContentLoaded',() => {
 	mask('[name = "phone"]');
 	checkTextInputs('[name = "name"]');
 	checkTextInputs('[name = "message"]');
+	showMoreStyles('.button-styles', '.styles-2');
 });
