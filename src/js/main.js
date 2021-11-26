19a6ca1cae550f24223edca278a6b699c9bd9edf
Your Name <you@example.com>
2021-11-26 15:19:52 +0100

    lesson-20

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import pictureSize from './modules/pictureSize';
 
 window.addEventListener('DOMContentLoaded',() => {
 	modals();
 	sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
 	sliders('.main-slider-item', 'vertical');
 	forms();
+	pictureSize('.sizes-block');
 });
